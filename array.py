9672d9e92106b35e0584612886080c15d2197fae
Pratik Radia <pratik@oneportals.com>
2020-01-26 18:09:09 +0530

    adding array.py and friends

---
+++ array.py
@@ -0,0 +1,13 @@
+import array as arr
+
+num_arr = arr.array('i')
+i=0
+val=0
+while i<6:
+    i+=1
+    print "enter your number:"
+    val=int(input())
+    num_arr.append(val)
+
+for val in num_arr:
+    print "%d " % val
---
+++ array2.py
@@ -0,0 +1,4 @@
+num_arr=list(map(int,raw_input('enter number;').split()))
+#num_arr.reverse()
+for val in num_arr:
+    print val,
